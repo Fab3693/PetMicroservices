d9ea8666f757d73e45d50c7256a046c55d2842d8
Alex <Aleksandr-aksinin98@mail.ru>
2025-09-04 18:18:55 +0700

    Revert "04.09"
    This reverts commit fdd0d995767564f43daf21941fba2d6c37138ac2.

--- service_c/src/main/java/mentorship/roadmap/microservices/service_c/ServiceCApplication.java
+++ service_c/src/main/java/mentorship/roadmap/microservices/service_c/ServiceCApplication.java
@@ -9,4 +9,5 @@ public class ServiceCApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceCApplication.class, args);
     }
+
 }
--- service_c/src/main/java/mentorship/roadmap/microservices/service_c/repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package mentorship.roadmap.microservices.service_c.repository;
-
-import mentorship.roadmap.microservices.service_c.entity.MessageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MessageRepository extends JpaRepository<MessageEntity, Long> {}
-
