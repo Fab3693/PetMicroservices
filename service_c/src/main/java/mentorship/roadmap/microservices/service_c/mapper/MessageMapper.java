abc20d0d733d965245878eb411579a0f94761fff
Alex <Aleksandr-aksinin98@mail.ru>
2025-09-18 14:06:49 +0700

    18.09 Third commit

--- service_c/src/main/java/mentorship/roadmap/microservices/service_c/mapper/MessageMapper.java
+++ service_c/src/main/java/mentorship/roadmap/microservices/service_c/mapper/MessageMapper.java
@@ -2,18 +2,10 @@ package mentorship.roadmap.microservices.service_c.mapper;
 
 import mentorship.roadmap.microservices.service_c.dto.MessageDto;
 import mentorship.roadmap.microservices.service_c.entity.MessageEntity;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
-@Component
-
-public class MessageMapper {
-
-    public MessageEntity toEntity(MessageDto dto) {
-        return new MessageEntity(
-                dto.getId(),
-                dto.getType(),
-                dto.getContent(),
-                dto.getCreatedAt()
-        );
-    }
+@Mapper
+public interface MessageMapper {
+    MessageEntity toEntity(MessageDto dto);
 }
